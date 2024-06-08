7ece432fbb0ef2845e2872545a11232bcd56f1af
Gayatri0603 <166007815+Gayatri0603@users.noreply.github.com>
2024-06-08 06:36:20 +0000

    config final

---
+++ .devcontainer/basic/operation.js
@@ -0,0 +1,10 @@
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter);
+let str1="hiiee"
+let str2="hello"
+let str3 = str1 + str2
+console.log(str3)
+console.log(1 + "2" + "2")
+console.log("1" + "2" + 3)
+console.log(1 + 2 + "3")
